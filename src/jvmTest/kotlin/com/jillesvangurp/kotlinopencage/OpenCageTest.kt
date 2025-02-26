bb4688a83cde6b41e67120e2316afe030b2abcb7
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2025-02-26 16:17:11 +0100

    add geocode test

--- src/jvmTest/kotlin/com/jillesvangurp/kotlinopencage/OpenCageTest.kt
+++ src/jvmTest/kotlin/com/jillesvangurp/kotlinopencage/OpenCageTest.kt
@@ -1,10 +1,12 @@
 package com.jillesvangurp.kotlinopencage
 
+import com.jillesvangurp.geojson.Geometry
 import com.jillesvangurp.geojson.latitude
 import com.jillesvangurp.geojson.longitude
 import io.kotest.matchers.doubles.shouldBeGreaterThan
 import io.kotest.matchers.doubles.shouldBeLessThan
 import io.kotest.matchers.ints.shouldBeGreaterThan
+import io.kotest.matchers.ints.shouldBeGreaterThanOrEqual
 import io.kotest.matchers.longs.shouldBeGreaterThan
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
@@ -13,11 +15,27 @@ import kotlinx.coroutines.runBlocking
 
 class OpenCageTest {
     val client by lazy {
-        OpencageClient(apiKey = openCageKey)
+        OpencageClient(
+            apiKey = openCageKey,
+            // makes it easy to dig out responses from logs for debugging
+            logging = true
+        )
     }
 
     @Test
-    fun `should geocode`() {
+    fun `should  geocode`() {
+        runBlocking {
+            val resp = client.geocode("wattstr 11", proximity = Geometry.Point.of(lat=52.54116,lon=13.39098))
+            resp.size shouldBeGreaterThanOrEqual 1
+            resp.results.first { it.components?.postcode == "13355"}.let { first ->
+                first.components?.city shouldBe "Berlin"
+                first.components?.suburb shouldBe "Gesundbrunnen"
+            }
+        }
+    }
+
+    @Test
+    fun `should reverse geocode`() {
         runBlocking {
             client.geocode(q="52.54125444670068, 13.390771722807354").let { resp->
                 println(DEFAULT_PRETTY_JSON.encodeToString(resp))
